e2bf98b4337f5dc337be424dda7ab0713454e827
cck-git <cck-git@nym.hush.com>
2022-09-30 20:14:38 +0300

    Added empty run.py to project root dir.

---
+++ run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Summary
+
+Description
+"""
+
+# --------------------
+# Imports
+# --------------------
+# Standard library modules:
+
+# Third party modules:
+
+# Local scripts and modules:
+
+# --------------------
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
