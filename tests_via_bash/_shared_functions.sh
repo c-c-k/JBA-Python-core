ef467f85cade4be3b2595c93a8a8a36fdfd3fb0e
c-c-k <cck-github@nym.hush.com>
2022-10-07 12:17:19 +0300

    Adjusted tests_via_bash/_shared_functions.sh.
    Added new line and separators to make it more clearly visible where one
    test ends and the next one starts.
    Made the test parameters bold to more easily visually separate them from
    the input.

--- tests_via_bash/_shared_functions.sh
+++ tests_via_bash/_shared_functions.sh
@@ -6,16 +6,20 @@
 PROJECTS_ROOT="../"
 
 function test_via_stdin() {
-  echo "TEST INPUTS: ""$*"
+  echo "-- TEST START --"
+  echo -e "\033[01mTEST INPUTS: ""$*\033[00m"
   TEST_INPUTS="$1"
   shift
   for TEST_INPUT ;do TEST_INPUTS="$TEST_INPUTS""\n""$TEST_INPUT"; done
   echo -ne "$TEST_INPUTS"|python3 "$TEST_SCRIPT_PATH"
+  echo -e "-- TEST END --\n"
 }
 
 function test_via_args() {
-  echo "TEST INPUTS: ""$*"
+  echo "-- TEST START --"
+  echo -e "\033[01mTEST INPUTS: ""$*\033[00m"
   python3 "$TEST_SCRIPT_PATH" "$@"
+  echo -e "-- TEST END --\n"
 }
 
 function print_test_info() {
