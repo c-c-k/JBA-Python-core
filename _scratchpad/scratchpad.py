4d218098e4ab31d76cf35dc5ae4074c4eeccfc96
c-c-k <cck-github@nym.hush.com>
2022-10-15 14:43:50 +0300

    Topic question solutions.

--- _scratchpad/scratchpad.py
+++ _scratchpad/scratchpad.py
@@ -8,16 +8,69 @@
 # feels like a waste of time, so instead I decided to use this script for
 # copying and pasting and editing the script and then copying the solution to
 # solutions_dump.
-# 
+#
 
-# Full name
-# Medium
-# 5154 users solved this problem. Latest completion was about 4 hours ago.
+
+# Temperature
+# Hard
+# 8114 users solved this problem. Latest completion was 1 day ago.
 # 
-# Rewrite the code below using the with keyword.
+# Below you can see a draft of a program for a basic temperature converter from
+# Fahrenheit to Celsius and vice versa. You need to finish the program by
+# creating a sort of entry point for the program in the function main.
 # 
-# Make sure to use the same variable names for file objects!
+# In this function, you should:
+# 
+# process the input. The input is a string containing a temperature value and a
+# unit of measurement which are separated by a whitespace character. We already
+# wrote the code that reads the input and creates the variables, but you still
+# need to figure out what the right type is for each of them and convert if
+# necessary;
+# call the appropriate function depending on what was given in the input;
+# print the converted temperature with the new unit.
+# 
+# Use the examples as a guide.
+# 
+# You do NOT need to call the function main.
 
 # ---- code start ----
-with open('name.txt') as f1, open('surname.txt') as f2, open('full_name.txt', 'w') as f3:
-    f3.write(' '.join([f1.read(), f2.read()]))
+from typing import Callable
+
+
+def fahrenheit_to_celsius(temps_f: float) -> float:
+    temps_c = (temps_f - 32) * 5 / 9
+    return round(temps_c, 2)
+
+
+def celsius_to_fahrenheit(temps_c: float) -> float:
+    temps_f = temps_c * 9 / 5 + 32
+    return round(temps_f, 2)
+
+
+def select_converter_function_for_unit(unit: str) -> Callable[[float], float]:
+    if unit == 'f':
+        return fahrenheit_to_celsius
+    else:
+        return celsius_to_fahrenheit
+
+
+def switch_unit(unit: str) -> str:
+    if unit == 'f':
+        return 'c'
+    else:
+        return 'f'
+
+
+def print_temperature(temperature: float, unit: str):
+    temperature = round(temperature, 1)
+    print(" ".join([str(temperature), unit]))
+
+
+def main():
+    """Entry point of the program."""
+    temperature, unit = input().split()  # read the input
+    temperature = float(temperature)
+    unit_converter = select_converter_function_for_unit(unit)
+    temperature = unit_converter(temperature)
+    unit = switch_unit(unit)
+    print_temperature(temperature, unit)
