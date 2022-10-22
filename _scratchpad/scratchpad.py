8dbe726db3849fa06d36cbe4cf4d46c188673ec4
c-c-k <cck-github@nym.hush.com>
2022-10-22 18:39:35 +0300

    Topic question solutions.

--- _scratchpad/scratchpad.py
+++ _scratchpad/scratchpad.py
@@ -10,42 +10,23 @@
 # solutions_dump.
 #
 
-# Odd or even?
+# Mr. and Ms. Smith
 # Medium
-# 844 users solved this problem. Latest completion was 19 minutes ago.
+# 1191 users solved this problem. Latest completion was about 4 hours ago.
 # 
-# There is a function called is_even(). We do not know its code, but we know
-# exactly how it works — given an integer, it returns True if the number is
-# even, and False if the number is odd. Write tests for this function (use
-# assertTrue and assertFalse).
+# Let's try to differentiate between some kinds of honorifics. Write a
+# regular expression in the variable template in such a way that it would
+# match the following strings:
 # 
-# In this Code Challenge, you do not need to import the module with the
-# function in the beginning of the code. To call the function, just type its
-# name, is_even().
+# Mr Smith
+# Mr. Smith
+# Ms Smith
+# Ms. Smith
+# 
+# Note that your template should not match any other strings (for example,
+# Dr. Smith).
 
 # ---- code start ----
-# tests for the is_even() function
-import unittest
-
-
-def is_even(num):
-    return num % 2 == 0
-
-
-class TestIsEven(unittest.TestCase):
-    __doc__: str
-
-    def test_when_output_true(self):
-        self.assertTrue(is_even(2))
-        self.__doc__ = self.__doc__
-
-    def test_when_output_false(self):
-        self.assertFalse(is_even(1))
-        self.__doc__ = self.__doc__
-
-    # def __repr__(self):
-    #     return str(self.test_when_output_false()) + str(self.test_when_output_true()) + "hello"
-
-
-if __name__ == '__main__':
-    unittest.main()
+import re
+# define regex template
+template = r'M[rs]\.? Smith'
