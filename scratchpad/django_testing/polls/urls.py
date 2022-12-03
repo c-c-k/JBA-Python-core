014cad7435434f1faf298daf732ead4f7ad837fd
c-c-k <cck-github@nym.hush.com>
2022-12-03 21:57:39 +0200

    Django testing - Django poll tutorial - views.

--- scratchpad/django_testing/polls/urls.py
+++ scratchpad/django_testing/polls/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('delete_question', views.delete_question, name='delete_question'),
     path('delete_choice', views.delete_choice, name='delete_choice'),
     path('repopulate', views.repopulate, name='repopulate'),
+    path('details/<int:question_id>', views.details, name='details'),
+    path('results/<int:question_id>', views.results, name='results'),
 ]
--- scratchpad/django_testing/polls/views.py
+++ scratchpad/django_testing/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import (
     render, redirect, get_object_or_404)
-# from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest
 # from django.views import View
 
 from .models import repopulate_polls, Choice, Question
@@ -20,12 +20,29 @@ def delete_question(request):
     return redirect("polls:index")
 
 
-def delete_choice(request):
-    choice = get_object_or_404(Choice, id=request.GET["choice_id"])
+def delete_choice(request: HttpRequest):
+    choice_id = request.GET["choice_id"]
+    source = request.GET["source"]
+    print(source)
+    choice = get_object_or_404(Choice, id=choice_id)
     choice.delete()
-    return redirect("polls:index")
+    return redirect(source)
 
 
 def repopulate(request):
     repopulate_polls()
     return redirect("polls:index")
+
+
+def details(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    context = {"question": question}
+    return render(request, "polls/results.html", context)
+
+
