6871598fa7e3259999e8e110aac7bb1f610be6e8
c-c-k <cck-github@nym.hush.com>
2022-12-05 20:30:57 +0200

    Django testing - Django poll tutorial - unit testing.

---
+++ scratchpad/django_testing/cs-django-turorial-5.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""Console Script: Django First app Tutorial part 5
+
+This is the script presented in part 5 of Django's first app tutorial,
+source at:
+https://docs.djangoproject.com/en/4.1/intro/tutorial05/
+
+It deals with experimenting with django's unit testing functions in the
+console before moving on to writing proper test unit's
+
+NOTE: This is a django console script (i.e. it is meant to:
+    1. Either be executed directly with pycharm's "run in python console"
+    option enabled in this script's run configuration.
+    2. Or to be imported into a new django console.
+    3. Or to be executed with pycharm ExecuteInConsole Action
+), in any case it requires django_manage_shell to have been activated.
+"""
+# --------------------
+# IMPORTS
+# --------------------
+# Standard library modules:
+
+# Third party modules:
+
+# Local scripts and modules:
+
+# --------------------
+# CONSTANTS
+# --------------------
+
+# --------------------
+# CLASSES
+# --------------------
+
+# --------------------
+# FUNCTIONS
+# --------------------
