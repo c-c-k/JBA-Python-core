4727e3a1792c729044413703cfc631570dfabb66
c-c-k <cck-github@nym.hush.com>
2022-12-09 22:35:19 +0200

    "Hyper School" django project: can't get the hang of inline Admin classes.

--- projects/current/hyper_school/HyperSchool/settings.py
+++ projects/current/hyper_school/HyperSchool/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'HyperSchool.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'HyperSchool.db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- projects/current/hyper_school/schedule/admin.py
+++ projects/current/hyper_school/schedule/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher, Course, Student
+
+
+class TeacherInline(admin.TabularInline):
+    can_delete = False
+    model = Teacher
+    readonly_fields = ('name', 'surname')
+
+
+class CourseInline(admin.TabularInline):
+    can_delete = False
+    model = Course
+    readonly_fields = ('title', 'duration_months')
+
+
+class StudentInline(admin.TabularInline):
+    can_delete = False
+    model = Student
+    readonly_fields = ('name', 'surname')
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    fields = ("__all__",)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ("__all__",)
+    # inlines = (StudentInline, )
+
+class StudentAdmin(admin.ModelAdmin):
+    fields = ("__all__",)
+    # inlines = (CourseInline, )
+
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Student, StudentAdmin)
