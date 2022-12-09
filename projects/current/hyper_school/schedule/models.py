19aff858ac59db5fd05523bc2d777bab6b21f86f
c-c-k <cck-github@nym.hush.com>
2022-12-09 19:52:47 +0200

    "Hyper School" django project: created Teacher,Course,Student models.

--- projects/current/hyper_school/schedule/models.py
+++ projects/current/hyper_school/schedule/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class PersonCommonInfo(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(PersonCommonInfo):
+    about = models.TextField
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    info = models.CharField(max_length=1024)
+    duration_months = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    teacher = models.ManyToManyField(Teacher)
+
+
+class Student(PersonCommonInfo):
+    course = models.ManyToManyField(Course)
