b9cf6733a1e00ded319251a814ec39ac67460949
c-c-k <cck-github@nym.hush.com>
2022-12-09 18:26:58 +0200

    "Hyper School" django project: adjusted settings.py.

--- projects/current/hyper_school/HyperSchool/settings.py
+++ projects/current/hyper_school/HyperSchool/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'HyperSchool.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'HyperSchool.db.sqlite3',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Israel'
 
 USE_I18N = True
 
